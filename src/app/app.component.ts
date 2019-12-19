f2de5e00ccfb5567b94c2e5984f60255ab285b63
home mac <admin@flotzam.com>
2019-12-18 23:02:16 -0800

    mirror

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
-import { MainComponent } from './main/main.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
@@ -12,18 +9,3 @@ import { ContactComponent } from './contact/contact.component';
 export class AppComponent {
   title = 'app'; 
 }
-
-export const routes: Routes = [
-  { 
-    path: '', 
-    component: MainComponent 
-  },
-  { 
-    path: 'about', 
-    component: AboutComponent 
-  },
-  { 
-    path: 'contact', 
-    component: ContactComponent 
-  }
-];
--- src/app/app.providers.ts
+++
@@ -1,7 +0,0 @@
-import { DataService } from './services/data.service';
-import { ConsumeService } from "./services/consume.service";
-
-export const APP_PROVIDERS = [
-    DataService,
-    ConsumeService
-];
